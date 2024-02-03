a4bd3dd2163200ba00ba71faa35446eafb2f4b60
Jimwel Dizon <jimweldizon.dev@gmail.com>
2024-02-03 22:03:42 +0800

    first commit

---
+++ frontend/app/api/topics/[id]/route.js
@@ -0,0 +1,34 @@
+import connectDb from "@/libs/mongoDb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectDb();
+  const topic = await Topic.findById(id);
+
+  return NextResponse.json({ topic }, { status: 200 });
+}
+
+export async function PUT(request, { params }) {
+  try {
+    const { id } = params;
+    const { title, description } = await request.json();
+    await connectDb();
+    await Topic.findByIdAndUpdate(id, { title, description });
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 400 });
+  }
+  return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const { id } = params;
+    await connectDb();
+    await Topic.findByIdAndDelete(id);
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 400 });
+  }
+  return NextResponse.json({ message: "Topic deleted" }, { status: 200 });
+}
---
+++ frontend/app/api/topics/route.js
@@ -0,0 +1,20 @@
+import connectDb from "@/libs/mongoDb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+  try {
+    const { title, description } = await request.json();
+    await connectDb();
+    await Topic.create({ title, description });
+  } catch (error) {
+    return NextResponse.json({ message: "Error" }, { status: 400 });
+  }
+  return NextResponse.json({ message: "Topic created" }, { status: 201 });
+}
+
+export async function GET() {
+  await connectDb();
+  const topics = await Topic.find();
+  return NextResponse.json({ topics }, { status: 200 });
+}
